f8793b1adfc1e4efd8ce1cdd8541ce65579de91e
j4ck41b3m <jorgeandr12@gmail.com>
2023-02-03 12:42:28 +0100

    more magnet

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Magnet : MonoBehaviour
 {
+    public GameObject blue, red;
+    public bool thing;
     // Start is called before the first frame update
     void Start()
     {
-        
+        thing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            thing = true;
+        }
+            if (thing)
+        {
+            blue.transform.Translate(red.transform.position);
+        }
     }
 }
